f7a92804fb43943104f6cd5fbe99b615eada00b9
ChevalierA73 <3e8ccecf@opayq.com>
2022-06-06 13:26:51 +0200

    fix kafka version

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,4 @@
-FROM 8u191-jdk-alpine3.9
+FROM openjdk:11.0.6-jre-slim-buster
+WORKDIR /app
+COPY target/deconz.jar /app.jar
+CMD ["java", "-jar", "-Dspring.profiles.active=default", "/app.jar"]
--- src/main/java/de/ritter/deconz/rest/SensorsApi.java
+++ src/main/java/de/ritter/deconz/rest/SensorsApi.java
@@ -4,8 +4,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.ritter.deconz.api.Sensors;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -18,16 +23,25 @@ import java.util.Iterator;
 @Slf4j
 @Component
 public class SensorsApi {
+    public static final String SENSORS = "sensors";
     @Value("${raspberry.pi}")
     private String address;
 
     @Value("${raspberry.apikey}")
     private String apikey;
 
+    @Value("${openweathermap.apikey}")
+    private String weatherMapApiKey;
+
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @Scheduled(fixedRate = 300000)
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Scheduled(fixedRate = 1800000)
     public void getAllSensors() throws IOException {
 
         RestTemplate restTemplate = new RestTemplate();
@@ -50,13 +64,36 @@ public class SensorsApi {
 
             Sensors sensors = sensorsMapper.readValue(sensorStr, Sensors.class);
 
-            kafkaTemplate.send("sensors", sensorStr);
+            kafkaTemplate.send(SENSORS, sensorStr);
 
             log.info("Sensor {} ", sensors);
         }
 
         log.info("sensors response {}", nodes);
 
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?id=2810834&appid=" + weatherMapApiKey + "&units=metric")
+                .get()
+                .build();
+
+        Response weaterMapApiresponse = client.newCall(request).execute();
+
+        String weatherResponse = weaterMapApiresponse.body().string();
+
+        log.info("current weather {}", weatherResponse);
+
+        kafkaTemplate.send(SENSORS, weatherResponse);
+
     }
+
+  /*  @KafkaListener(topics = SENSORS, groupId = "foo")
+    public void listenWithHeaders(
+            @Payload String message,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
+       log.info(
+                "Received Message: {} from partition: {}", message, partition);
+    }*/
 
 }
